36f9d35da624936d03dbf05c2dcba4b8cdabfb06
rizwankarim <rizusamnani@gmail.com>
2021-01-20 16:27:18 +0500

    Main Switch

--- app/src/main/java/com/masterandroid/geofencing_sharedpref/MainActivity.java
+++ app/src/main/java/com/masterandroid/geofencing_sharedpref/MainActivity.java
@@ -1,14 +1,161 @@
 package com.masterandroid.geofencing_sharedpref;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GeofencingClient geofencingClient;
+    private GeofenceHelper geofenceHelper;
+    private String GEOFENCE_ID = "SOME_GEOFENCE_ID";
+    Button press;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        press= findViewById(R.id.press);
+        press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableUserLocation();
+            }
+        });
+
+        geofencingClient = LocationServices.getGeofencingClient(this);
+        geofenceHelper = new GeofenceHelper(this);
+
+    }
+
+    private void enableUserLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //mMap.setMyLocationEnabled(true);
+            getCurrentLocation();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 10001);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 10001);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 10001) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                }
+                //mMap.setMyLocationEnabled(true);
+                getCurrentLocation();
+            }
+        }
+
+        if (requestCode == 10002) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                }
+                Toast.makeText(this,"You can add geofences...",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this,"Background location access is necessary for geofences to trigger...",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private void addGeofence(LatLng latLng, float radius) {
+        Geofence geofence = geofenceHelper.getGeofence(GEOFENCE_ID, latLng, radius, Geofence.GEOFENCE_TRANSITION_ENTER
+                | Geofence.GEOFENCE_TRANSITION_DWELL | Geofence.GEOFENCE_TRANSITION_EXIT);
+        GeofencingRequest geofenceRequest = geofenceHelper.getGeofenceRequest(geofence);
+        PendingIntent pendingIntent = geofenceHelper.getPendingIntent();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        geofencingClient.addGeofences(geofenceRequest, pendingIntent).
+                addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("MapsActivity","onSuccess..");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        String errorMessage= geofenceHelper.getErrorCode(e);
+                        Log.d("MapsActivity","onFailure:" + errorMessage);
+                    }
+                });
+    }
+
+    private void getCurrentLocation() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(this)
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                .removeLocationUpdates(this);
+                        if (locationResult != null && locationResult.getLocations().size() > 0) {
+                            int locationindex = locationResult.getLocations().size() - 1;
+                            double current_lat = locationResult.getLocations().get(locationindex).getLatitude();
+                            double current_long = locationResult.getLocations().get(locationindex).getLongitude();
+                            LatLng myLatlng = new LatLng(current_lat, current_long);
+//                            mMap.clear();
+//                            addMarker(myLatlng);
+//                            addCircle(myLatlng,200);
+//                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatlng,16));
+                            addGeofence(myLatlng,200);
+//                            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+//
+//                            if (!sharedPrefs.contains("mylist")) {
+//                                Log.d("Dont Exists", "creating");
+//                                ArrayList<LatLng> latlongList = new ArrayList<>();
+//                                latlongList.add(myLatlng);
+//                                saveArrayList(context, latlongList, "mylist");
+//                            } else {
+//                                Log.d("Exists", "updating");
+//                                ArrayList<LatLng> latlongList = getArrayList(context, "mylist");
+//                                latlongList.add(myLatlng);
+//                                saveArrayList(context, latlongList, "mylist");
+//                            }
+
+                            Log.d("Receiver", String.valueOf(current_lat) + "," + String.valueOf(current_long));
+                        }
+                    }
+                }, Looper.getMainLooper());
     }
 }
